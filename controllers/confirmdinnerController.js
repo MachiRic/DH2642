277ff606ccec2404172cbb39a37d149e9b8517a6
mrich <mrich@kth.se>
2019-02-07 11:18:13 +0100

    Lab3 better navigation
    Fixed navigation buttons when error occurs

--- controllers/confirmdinnerController.js
+++
@@ -1,7 +0,0 @@
-class ConfirmDinnerController {
-    constructor(container, generalController, model){
-        this.generalController = generalController;
-        this.view = new ConfirmdinnerView(container, model);
-        this.view.button.addEventListener("click", () => this.generalController.showPrintview());
-    }
-}
--- controllers/headerController.js
+++
@@ -1,6 +0,0 @@
-class HeaderController {
-    constructor( generalController, view) {
-        this.generalController = generalController;
-        view.headbutton.addEventListener("click", () => this.generalController.showHomeview());
-    }
-}
